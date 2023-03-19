526b57d8ac059defef5d55cfe0f61d345be95cf0
Adam <kalbouneha@cardiff.ac.uk>
2023-03-19 15:34:32 +0000

    Use of Json Object to get from Flask Server

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+  const [data, setData] = useState([{}])
+
+  useEffect(()=> {
+    fetch("/home").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(typeof data.Home == 'undefined')? (
+        <p>Loading...</p>
+      ) : (
+        data.Home.map((Home, i) => (
+          <p key={i}>{Home}</p>
+        ))
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
