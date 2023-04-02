739da15b10b94a3257fc9ef6a1fd83f14ea9b8d5
Marwa <omarfm@cardiff.ac.uk>
2023-04-02 17:25:15 +0100

    added grids and placeholders for home page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,12 @@
 function NavBar() {
     return (
-        <div style={{display: 'block', position: 'absolute', marginLeft: 'auto', marginTop: '15%'}}>
-            <div style={{display:'flex', flexDirection: 'column', border: '1px solid white', height: '500px', width: '100px'}}>
-            <span>Home</span>
-            <span>Login</span>
-        </div>
-        </div>
+        <div className="my-32 flex min-h-full w-32 flex-col items-center px-5">
+                <div>LOGO</div>
+                <div>Login</div>
+                <div>Login</div>
+                <div>Login</div>
+              </div>
+        
     )
 }
 
