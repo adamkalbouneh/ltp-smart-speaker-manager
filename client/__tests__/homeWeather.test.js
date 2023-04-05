3200a990c7a560ddc5d1bce8c8febb8ed71f9c8d
c2061470 <omarfm@cardiff.ac.uk>
2023-04-05 14:23:31 +0100

    Updated file

--- client/__tests__/homeWeather.test.js
+++ client/__tests__/homeWeather.test.js
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import App from './App';
+import HomePageWeather from './HomePageWeather';
 
 jest.mock('axios', () => ({
   get: jest.fn(() => Promise.resolve({ data: { temperature: 20, precipitation: 5 } }))
 }));
 
-describe('App', () => {
+describe('HomePageWeather', () => {
   it('displays the temperature and precipitation from the API', async () => {
-    render(<App />);
+    render(<HomePageWeather />);
     const temperatureElement = await screen.findByText(/Temperature:/);
     const precipitationElement = await screen.findByText(/Precipitation:/);
 
