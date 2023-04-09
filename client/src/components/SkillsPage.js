624d3a526e574cdde8892bebd9d27a84bfc8472e
C2048062 <shahina@cardiff.ac.uk>
2023-04-09 19:27:48 +0100

    working prototype for skill

--- client/src/components/SkillsPage.js
+++ client/src/components/SkillsPage.js
@@ -5,6 +5,7 @@ const SkillsPage = () => {
   const [selectedOption, setSelectedOption] = useState(
     parseInt(localStorage.getItem("selectedOption")) || 0
   );
+  const [sliderValue, setSliderValue] = useState(50);
 
   useEffect(() => {
     const deviceSelect = document.getElementById("device-select");
@@ -38,6 +39,10 @@ const SkillsPage = () => {
     setSelectedOption(optionValue);
   };
 
+  const handleSliderChange = (event) => {
+    setSliderValue(event.target.value);
+  };
+
   return (
     <div className="page">
       <DashboardHeader />
@@ -54,7 +59,44 @@ const SkillsPage = () => {
         </select>
       </div>
 
-      <button className="blue-button content" id="submit-button">Submit</button>
+      <button className="blue-button content" id="submit-button">
+        Submit
+      </button>
+
+      <div className="skill-container">
+        <div className="skill-head">
+          <h2>Skill</h2>
+          <input
+            type="checkbox"
+            id="enable-checkbox"
+            name="enable-checkbox"
+          />
+          <label htmlFor="enable-checkbox">Enable skill</label>
+        </div>
+        <div className="skill-body">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+            fugiat iusto fuga praesentium optio, eaque rerum!{" "}
+          </p>
+          <div className="slider-container">
+            <label htmlFor="frequency-slider">Frequency:</label>
+            <div className="slidecontainer">
+              <input
+                type="range"
+                min="1"
+                max="5"
+                value={sliderValue}
+                className="slider"
+                id="myRange"
+                onChange={handleSliderChange}
+              />
+            </div>
+            <output htmlFor="frequency-slider">{sliderValue}</output>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
