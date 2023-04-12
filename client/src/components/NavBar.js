0f097973c8061f6e28c3478cbd522166361702b6
c2061470 <omarfm@cardiff.ac.uk>
2023-04-12 19:07:31 +0100

    css nav bar - no connection yet

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightFromBracket, faRobot, faListUl, faLink } from "@fortawesome/free-solid-svg-icons";
+
+
+
 function NavBar() {
+
     return (
-        <div className="my-32 flex min-h-full w-32 flex-col items-center px-5">
-                <div>LOGO</div>
-                <div>Login</div>
-                <div>Login</div>
-                <div>Login</div>
+        <div className="navBox my-32 flex justify-between min-h-full w-32 flex-col items-center px-5">
+                <div>
+                <button type="button" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-1 text-center mr-2 mb-2"><span>Skills</span><FontAwesomeIcon size="md" icon={faRobot} /></button>
+                <button type="button" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-1 text-center mr-2 mb-2"><span>Routines</span><FontAwesomeIcon size="md" icon={faListUl}/></button>
+                <button type="button" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-1 text-center mr-2 mb-2"><span>Pair</span><FontAwesomeIcon size="md" icon={faLink}/></button>
+                </div>
+                <div><button type="button" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-1 text-center mr-2 mb-2"><span>Log out</span><FontAwesomeIcon size="md" icon={faArrowRightFromBracket}/></button></div>
               </div>
         
     )
