8a61c02d4d818f31e04cce38b3f1d3de31aa6c2f
c2061470 <omarfm@cardiff.ac.uk>
2023-04-14 01:56:07 +0100

    Arranged homepage to be a notice

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -7,6 +7,8 @@ import HomePageStatus from "./HomePageStatus";
 import NavBar from "./NavBar";
 import DashboardHeader from "./DashboardHeader";
 import IntroductionSection from "./HPIntro";
+import HPAccordion from "./HPAccordion";
+import HPSuggestions from "./HPSuggestions";
 
 const HomePage = () => {
     return (
@@ -15,9 +17,10 @@ const HomePage = () => {
                 {/* Top NAV */}
                 <div>
                     <div className="pt-4 ">
-                        <div className="mb-4 flex h-20 items-center justify-between bg-indigo-800 px-5">
+
+                        <div className="mb-4 flex h-20 items-center justify-between bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-md text-md px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl">
                             <div>LOGO</div>
-                            <div></div>
+                            <div style={{ fontSize: "30px", paddingLeft: "100px" }}>    Home </div>
                             <div>Marwa</div>
                         </div>
                     </div>
@@ -45,21 +48,10 @@ const HomePage = () => {
                             {/* Section to welcome user*/}
                             {/* the css for the gradient text box */}
                             <div className="text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl textBox ">
-                                <div className="text-2xl">Welcome</div>
-                                <div className="text-lg">
-                                    User, welcome to the website da da da{" "}
-                                </div>
-                            </div>
+                                <div className="text-2xl">Speaker Features</div>
 
-                            {/* the css for the gradient text box */}
-                            <div className="text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl textBox ">
-                                {/* Section to welcome user*/}
-                                <div className="flex h-full flex-col items-center justify-center">
-                                    <div className="text-2xl">Welcome</div>
-                                    <div className="text-lg">
-                                        User, welcome to the website da da da{" "}
-                                    </div>
-                                </div>
+                                <HPAccordion />
+
                             </div>
                             <div className="">
                                 {/* Weather widget, Flex, First box is current weather, second box is 5 days forcast */}
@@ -67,6 +59,17 @@ const HomePage = () => {
                                     <HomePageWeather />
                                 </div>
                             </div>
+
+                            {/* the css for the gradient text box */}
+                            <div className="text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl textBox ">
+                                {/* Section to welcome user*/}
+                                <div className="flex h-full flex-col items-center justify-center">
+                                    <div className="text-2xl">Voice Command Suggestions</div>
+                                    
+                                    <HPSuggestions />
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
