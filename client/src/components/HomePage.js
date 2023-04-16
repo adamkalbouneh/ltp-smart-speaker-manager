4169e43400e8cf305b0f796f33579a6044b61f30
c2061470 <omarfm@cardiff.ac.uk>
2023-04-16 13:15:11 +0100

    css changes

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Container } from "@mantine/core";
 import { Grid } from "@mantine/core";
 import "../styling/Home.css"
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import HomePageWeather from "./HomePageWeather";
 import HomePageStatus from "./HomePageStatus";
 import NavBar from "./NavBar";
@@ -53,9 +54,10 @@ const HomePage = () => {
                                 <HPAccordion />
 
                             </div>
+                            
                             <div className="">
                                 {/* Weather widget, Flex, First box is current weather, second box is 5 days forcast */}
-                                <div className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl  ">
+                                <div className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl textBox ">
                                     <HomePageWeather />
                                 </div>
                             </div>
@@ -65,12 +67,81 @@ const HomePage = () => {
                                 {/* Section to welcome user*/}
                                 <div className="flex h-full flex-col items-center justify-center">
                                     <div className="text-2xl">Voice Command Suggestions</div>
-                                    
+
                                     <HPSuggestions />
                                 </div>
+
                             </div>
 
+                            <div className="text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl textBox ">
+                                <div className="text-2xl">Frequently Asked Questions</div>
+
+                                <HPAccordion />
+
+                            </div>
+
+                           
+
                         </div>
+                        <div className="flex w-full flex-col gap-6 col-span-2 items-center justify-between bg-gradient-to-br from-team-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-md text-md px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl">
+                                {/* Weather widget, Flex, First box is current weather, second box is 5 days forcast */}
+                                <button type="button" className="text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl flex w-full flex-col gap-6 col-span-2  ">
+                                    
+                                <div className="text-2xl">View Speaker Dashboard</div>
+                                </button>
+                            </div>
+                            <div className="grid h-full w-full grid-flow-col grid-cols-2 grid-rows-3 gap-20">
+                        <div className="text-white bg-gradient-to-br from-cyan-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl textBox ">
+                                {/* Section to welcome user*/}
+                                <div className="flex h-full flex-col items-center justify-center">
+                                    <div className="text-2xl">Daily Check in</div>
+
+                                    <div className="text-lg">
+                                            Here is a look into how your loved ones are manging{" "}
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="text-white bg-gradient-to-br from-yellow-600 to-red-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl textBox ">
+                                {/* Section to welcome user*/}
+                                <div className="flex h-full flex-col items-center justify-center">
+                                    <div className="text-2xl">Weekly Schedule</div>
+
+                                    
+                                    <div className="text-lg">
+                                            Here is a look into what your loved ones has planned for the week{" "}
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="text-white bg-gradient-to-br from-yellow-600 to-purple-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl textBox ">
+                                {/* Section to welcome user*/}
+                                <div className="flex h-full flex-col items-center justify-center">
+                                    <div className="text-2xl">News</div>
+
+                                    <div className="text-lg">
+                                            View the news stations they follow{" "}
+                                        </div>
+
+                                </div>
+                            </div>
+                        <div className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 textBox ">
+                                {/* Section to welcome user*/}
+                                <div className="flex h-full flex-col items-center justify-center">
+                                    <div className="text-2xl">Music</div>
+
+                                    <div className="text-lg">
+                                            View what's "Playing Now" and what they have been listening to{" "}
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="">
+                                {/* Weather widget, Flex, First box is current weather, second box is 5 days forcast */}
+                                <div className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 rounded-2xl textBox ">
+                                    <HomePageWeather />
+                                </div>
+                            </div>
+                            </div>
                     </div>
                 </div>
             </div >
