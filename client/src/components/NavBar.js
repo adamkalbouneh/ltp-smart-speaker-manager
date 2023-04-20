449930a2edd76e65b4c52d82087e9f72b631e9bd
c2061470 <omarfm@cardiff.ac.uk>
2023-04-20 13:58:04 +0100

    Added pages into navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,18 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightFromBracket, faRobot, faListUl, faLink, faTableColumns } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightFromBracket, faRobot, faListUl, faLink, faTableColumns, faHome } from "@fortawesome/free-solid-svg-icons";
 import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <div className="navBox my-0 flex justify-between min-h-full w-32 flex-col items-center px-5">
+        <div className="navBox my-0 flex justify-between h-25 w-32 flex-col items-center px-5">
             <div>
-                <Link to="/login" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-1 text-center mr-2 mb-2">
-                    <span>Skills</span>
-                    <FontAwesomeIcon size="md" icon={faRobot} />
+            <Link to="/home" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-1 text-center mr-2 mb-2">
+                    <span>Home</span>
+                    <FontAwesomeIcon size="md" icon={faHome} />
                 </Link>
                 <Link to="/dashboard" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-1 text-center mr-2 mb-2">
                     <span>Dashboard</span>
                     <FontAwesomeIcon size="md" icon={faTableColumns} />
+                </Link>
+                <Link to="/login" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-1 text-center mr-2 mb-2">
+                    <span>Skills</span>
+                    <FontAwesomeIcon size="md" icon={faRobot} />
                 </Link>
                 <Link to="/pair" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-1 text-center mr-2 mb-2">
                     <span>Routines</span>
