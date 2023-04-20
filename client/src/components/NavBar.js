a5796a55fa861e050c6bec254e1d3f5545b86a34
Milos Vujovic <vujovicm@cardiff.ac.uk>
2023-04-20 17:33:29 +0100

    upfated jest test and account page styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightFromBracket, faRobot, faListUl, faLink, faTableColumns, faHome } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightFromBracket, faRobot, faListUl, faLink, faTableColumns, faHome, faUser} from "@fortawesome/free-solid-svg-icons";
 import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
         <div className="navBox my-0 flex justify-between h-25 w-35 flex-col items-center px-5">
             <div>
-            <Link to="/home" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                <Link to="/home" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
                     <span>Home</span>
                     <FontAwesomeIcon size="md" icon={faHome} />
                 </Link>
@@ -26,7 +26,10 @@ function NavBar() {
                     <span>Pair</span>
                     <FontAwesomeIcon size="md" icon={faLink} />
                 </Link>
-                
+                <Link to="/account" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                    <span>Account</span>
+                    <FontAwesomeIcon size="md" icon={faUser} />
+                </Link>      
             </div>
             <div>
                 <Link to="/login" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
