5eec1dac91dd587b31dd5ec1f5df1a85534f4b14
Ahmad Shahin <shahina@cardiff.ac.uk>
2023-04-20 23:40:40 +0000

    Revert "Merge branch 'css-skills' of https://git.cardiff.ac.uk/c2050237/ltp-smart-speaker-manager into Dev"
    This reverts merge request !41

--- client/src/components/SideNavbar.js
+++ client/src/components/SideNavbar.js
@@ -1,195 +1,73 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowRightFromBracket,
-  faRobot,
-  faListUl,
-  faLink,
-  faTableColumns,
-  faHome,
-  faUser,
-  faClock,
-  faQuestion,
-} from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
-import io from "socket.io-client";
-import connectedImage from "../img/connected_image.png";
-import disconnectedImage from "../img/disconnected_image.png";
-import React, { useEffect, useState } from "react";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightFromBracket, faRobot, faListUl, faLink, faTableColumns, faHome, faUser, faClock, faQuestion, faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import VolumeControl from './VolumeControl';
 
 function SideNavbar() {
-  const [connected, setConnected] = useState(false);
+    const [showVolumeDropdown, setShowVolumeDropdown] = useState(false);
 
-  useEffect(() => {
-    const socket = io("http://localhost:3000");
-    socket.on("mycroft_connected", (data) => {
-      console.log("Received mycroft_connected event:", data);
-      setConnected(true);
-    });
-
-    return () => {
-      socket.disconnect();
+    const toggleVolumeDropdown = () => {
+        setShowVolumeDropdown(!showVolumeDropdown);
     };
-  }, []);
 
-  return (
-    <div className="navBox my-0 flex justify-between h-25 w-38 flex-col items-center px-5">
-      <div>
-        <Link
-          to="/home"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Home</span>
-          <FontAwesomeIcon size="md" icon={faHome} />
-        </Link>
-        <Link
-          to="/dashboard"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Dashboard</span>
-          <FontAwesomeIcon size="md" icon={faTableColumns} />
-        </Link>
-        <Link
-          to="/skills"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Skills</span>
-          <FontAwesomeIcon size="md" icon={faRobot} />
-        </Link>
-        <Link
-          to="/routine"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Routines</span>
-          <FontAwesomeIcon size="md" icon={faListUl} />
-        </Link>
-        <Link
-          to="/alarms"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Alarms</span>
-          <FontAwesomeIcon size="md" icon={faClock} />
-        </Link>
-        <Link
-          to="/prompt"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Ask Mycroft</span>
-          <FontAwesomeIcon size="md" icon={faQuestion} />
-        </Link>
-        <Link
-          to="/account"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Account</span>
-          <FontAwesomeIcon size="md" icon={faUser} />
-        </Link>
-      </div>
-      <div className="nav-icon">
-        <img
-          src={connected ? connectedImage : disconnectedImage}
-          alt="Connection Status"
-        />
-      </div>
-      <div>
-        <Link
-          to="/login"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Log out</span>
-          <FontAwesomeIcon size="md" icon={faArrowRightFromBracket} />
-        </Link>
-      </div>
-    </div>
-  );
-  const [showVolumeDropdown, setShowVolumeDropdown] = useState(false);
-
-  const toggleVolumeDropdown = () => {
-    setShowVolumeDropdown(!showVolumeDropdown);
-  };
-
-  return (
-    <div className="navBox my-0 flex justify-between h-25 w-38 flex-col items-center px-5">
-      <div>
-        <Link
-          to="/home"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Home</span>
-          <FontAwesomeIcon size="md" icon={faHome} />
-        </Link>
-        <Link
-          to="/dashboard"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Dashboard</span>
-          <FontAwesomeIcon size="md" icon={faTableColumns} />
-        </Link>
-        <Link
-          to="/skills"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Skills</span>
-          <FontAwesomeIcon size="md" icon={faRobot} />
-        </Link>
-        <Link
-          to="/routine"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Routines</span>
-          <FontAwesomeIcon size="md" icon={faListUl} />
-        </Link>
-        <Link
-          to="/alarms"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Alarms</span>
-          <FontAwesomeIcon size="md" icon={faClock} />
-        </Link>
-        <Link
-          to="/prompt"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Ask Mycroft</span>
-          <FontAwesomeIcon size="md" icon={faQuestion} />
-        </Link>
-        <Link
-          to="/account"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Account</span>
-          <FontAwesomeIcon size="md" icon={faUser} />
-        </Link>
-        {/* Modified Link for Device Volume */}
-        <div className="relative">
-          <button
-            onClick={toggleVolumeDropdown}
-            className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-          >
-            <span>Device Volume</span>
-            <FontAwesomeIcon icon={faVolumeHigh} />
-          </button>
-          {showVolumeDropdown && (
-            <div className="absolute left-0 mt-2 rounded-md shadow-lg bg-white z-10">
-              <div className="rounded-md">
-                <VolumeControl />
-              </div>
+    
+    return (
+        <div className="navBox my-0 flex justify-between h-25 w-38 flex-col items-center px-5">
+            <div>
+                <Link to="/home" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                    <span>Home</span>
+                    <FontAwesomeIcon size="md" icon={faHome} />
+                </Link>
+                <Link to="/dashboard" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                    <span>Dashboard</span>
+                    <FontAwesomeIcon size="md" icon={faTableColumns} />
+                </Link>
+                <Link to="/skills" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                    <span>Skills</span>
+                    <FontAwesomeIcon size="md" icon={faRobot} />
+                </Link>
+                <Link to="/routine" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                    <span>Routines</span>
+                    <FontAwesomeIcon size="md" icon={faListUl} />
+                </Link>
+                <Link to="/alarms" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                    <span>Alarms</span>
+                    <FontAwesomeIcon size="md" icon={faClock} />
+                </Link> 
+                <Link to="/prompt" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                    <span>Ask Mycroft</span>
+                    <FontAwesomeIcon size="md" icon={faQuestion} />
+                </Link>
+                <Link to="/account" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                    <span>Account</span>
+                    <FontAwesomeIcon size="md" icon={faUser} />
+                </Link>   
+                  {/* Modified Link for Device Volume */}
+             <div className="relative">
+                    <button onClick={toggleVolumeDropdown} className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                        <span>Device Volume</span>
+                        <FontAwesomeIcon icon={faVolumeHigh} />
+                    </button>
+                    {showVolumeDropdown && (
+                        <div className="absolute left-0 mt-2 rounded-md shadow-lg bg-white z-10">
+                            <div className="rounded-md">
+                                <VolumeControl />
+                            </div>
+                        </div>
+                    )}
+                </div>
+                {/* End of Device Volume */}  
             </div>
-          )}
+        
+            <div>
+                <Link to="/login" className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2">
+                    <span>Log out</span>
+                    <FontAwesomeIcon size="md" icon={faArrowRightFromBracket} />
+                </Link>
+            </div>
         </div>
-        {/* End of Device Volume */}
-      </div>
-
-      <div>
-        <Link
-          to="/login"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Log out</span>
-          <FontAwesomeIcon size="md" icon={faArrowRightFromBracket} />
-        </Link>
-      </div>
-    </div>
-  );
+    )
 }
 
 export default SideNavbar;
--- client/src/pages/PromptPage.js
+++ client/src/pages/PromptPage.js
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
-import "../styling/Prompt.css";
-import axios from "axios";
-import NavBar from "../components/SideNavbar";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import React, { useState } from 'react';
+import '../styling/Prompt.css';
+import axios from 'axios';
 
 const Prompt = () => {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
   const handleChange = (e) => {
     setMessage(e.target.value);
@@ -14,44 +12,28 @@ const Prompt = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("/send-message", { message });
+      const response = await axios.post('/send-message', { message });
       alert(response.data.message);
-      setMessage("");
+      setMessage('');
     } catch (error) {
-      alert("Error: " + error.message);
+      alert('Error: ' + error.message);
     }
   };
 
   return (
     <div>
-      <div className=" min-h-screen bg-indigo-950 text-lg text-white">
-        {/* Top NAV */}
-        <div>
-          <div className="mb-4 flex h-20 items-center justify-between bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-md text-md px-5 py-2.5 text-center rounded-2xl">
-            <Link to="/home" className="logo-box-image" />
-            <div style={{ fontSize: "30px", paddingLeft: "100px" }}>Skills</div>
-            <div>Marwa</div>
-          </div>
-        </div>
-        {/* Left Side Bar Nav */}
-        <div className="flex mt-10">
-          <NavBar />
-          <div id="prompt">
-            <h1>Prompt Mycroft</h1>
-            <form onSubmit={handleSubmit} className="promptForm">
-              <textarea
-                className="messageInput"
-                value={message}
-                onChange={handleChange}
-                placeholder="Enter your message here..."
-              />
-              <button type="submit" className="submitButton">
-                Send Message
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
+      <h1>Prompt Mycroft</h1>
+      <form onSubmit={handleSubmit} className="promptForm">
+        <textarea
+          className="messageInput"
+          value={message}
+          onChange={handleChange}
+          placeholder="Enter your message here..."
+        />
+        <button type="submit" className="submitButton">
+          Send Message
+        </button>
+      </form>
     </div>
   );
 };
