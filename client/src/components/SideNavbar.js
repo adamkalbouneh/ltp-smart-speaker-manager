f9c19c1fed5e2671549c3f3adbb4301e72bb609a
C2048062 <shahina@cardiff.ac.uk>
2023-04-21 00:57:30 +0100

    skills css added, volume added back

--- client/src/components/SideNavbar.js
+++ client/src/components/SideNavbar.js
@@ -9,14 +9,22 @@ import {
   faUser,
   faClock,
   faQuestion,
+  faVolumeHigh,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import io from "socket.io-client";
 import connectedImage from "../img/connected_image.png";
 import disconnectedImage from "../img/disconnected_image.png";
 import React, { useEffect, useState } from "react";
+import VolumeControl from "./VolumeControl";
 
 function SideNavbar() {
+  const [showVolumeDropdown, setShowVolumeDropdown] = useState(false);
+
+  const toggleVolumeDropdown = () => {
+    setShowVolumeDropdown(!showVolumeDropdown);
+  };
+
   const [connected, setConnected] = useState(false);
 
   useEffect(() => {
@@ -30,82 +38,6 @@ function SideNavbar() {
       socket.disconnect();
     };
   }, []);
-
-  return (
-    <div className="navBox my-0 flex justify-between h-25 w-38 flex-col items-center px-5">
-      <div>
-        <Link
-          to="/home"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Home</span>
-          <FontAwesomeIcon size="md" icon={faHome} />
-        </Link>
-        <Link
-          to="/dashboard"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Dashboard</span>
-          <FontAwesomeIcon size="md" icon={faTableColumns} />
-        </Link>
-        <Link
-          to="/skills"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Skills</span>
-          <FontAwesomeIcon size="md" icon={faRobot} />
-        </Link>
-        <Link
-          to="/routine"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Routines</span>
-          <FontAwesomeIcon size="md" icon={faListUl} />
-        </Link>
-        <Link
-          to="/alarms"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Alarms</span>
-          <FontAwesomeIcon size="md" icon={faClock} />
-        </Link>
-        <Link
-          to="/prompt"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Ask Mycroft</span>
-          <FontAwesomeIcon size="md" icon={faQuestion} />
-        </Link>
-        <Link
-          to="/account"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Account</span>
-          <FontAwesomeIcon size="md" icon={faUser} />
-        </Link>
-      </div>
-      <div className="nav-icon">
-        <img
-          src={connected ? connectedImage : disconnectedImage}
-          alt="Connection Status"
-        />
-      </div>
-      <div>
-        <Link
-          to="/login"
-          className="flex items-center gap-2 text-white bg-gradient-to-br from-teal-600 to-indigo-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-2 py-1 text-center mr-2 mb-2"
-        >
-          <span>Log out</span>
-          <FontAwesomeIcon size="md" icon={faArrowRightFromBracket} />
-        </Link>
-      </div>
-    </div>
-  );
-  const [showVolumeDropdown, setShowVolumeDropdown] = useState(false);
-
-  const toggleVolumeDropdown = () => {
-    setShowVolumeDropdown(!showVolumeDropdown);
-  };
 
   return (
     <div className="navBox my-0 flex justify-between h-25 w-38 flex-col items-center px-5">
@@ -177,6 +109,12 @@ function SideNavbar() {
           )}
         </div>
         {/* End of Device Volume */}
+        <div className="nav-icon">
+          <img
+            src={connected ? connectedImage : disconnectedImage}
+            alt="Connection Status"
+          />
+        </div>
       </div>
 
       <div>
